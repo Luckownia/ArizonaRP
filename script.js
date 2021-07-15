8f3d89aa84e2d53ea9516686d1a940e416274cc2
Filip Kubala <69809370+Luckownia@users.noreply.github.com>
2021-07-15 18:05:17 +0200

    Update script.js (function custom cursor)

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-            const navLinks = document.querySelectorAll(".nav-link");
-            navLinks.forEach(link => {
-
-                link.addEventListener("click", () => {
-                    navLinks.forEach(link1 => {
-                        if (link1.classList.contains('active')) {
-                            link1.classList.remove('active');
-                        }
-                    })
-                    link.classList.add("active");
-                })
-            })
-
             const inputCheckbox = document.querySelector(".switch-mode-checkbox");
             const circleImage = document.querySelector(".circle-image")
             const logoPalm = document.querySelector(".logo img");
@@ -31,7 +18,55 @@
                 }
             });
 
-            const btnMore = document.querySelector(".btn-more");
+        
+            const headline = document.querySelector(".headline");
+            const subHeadline = document.querySelector(".sub-headline");
+            const logo = document.querySelector(".logo");
+            const navLabel = document.querySelector("label");
+            const cursor = document.querySelector(".mouse");
+
+            const animateCursor = ()=>{
+                 cursor.classList.add("cursor-animate");
+            }
+
+            const removeAnimateCursor = ()=>{
+                 cursor.classList.remove("cursor-animate");
+            }
+            headline.addEventListener("mouseenter",animateCursor)
+            headline.addEventListener("mouseleave",removeAnimateCursor)
+            subHeadline.addEventListener("mouseenter",animateCursor)
+            subHeadline.addEventListener("mouseleave",removeAnimateCursor)
+            logo.addEventListener("mouseenter",animateCursor)
+            logo.addEventListener("mouseleave",removeAnimateCursor)
+            navLabel.addEventListener("mouseenter",animateCursor)
+            navLabel.addEventListener("mouseleave",removeAnimateCursor)
+
+
+            window.addEventListener("mousemove",(e)=>{
+                let x = e.clientX;
+                let y = e.clientY;
+
+                cursor.style.top = y ;
+                cursor.style.left = x ;
+            });
+
+              const navLinks = document.querySelectorAll(".nav-link");
+            navLinks.forEach(link => {
+
+                link.addEventListener("click", () => {
+                    navLinks.forEach(link1 => {
+                        if (link1.classList.contains('active')) {
+                            link1.classList.remove('active');
+                        }
+                    })
+                    link.classList.add("active");
+                })
+
+               link.addEventListener("mouseenter",animateCursor)
+                link.addEventListener("mouseleave",removeAnimateCursor)
+            })
+
+                const btnMore = document.querySelector(".btn-more");
             btnMore.addEventListener("mouseenter", (e) => {
                 let x = e.clientX - e.target.offsetLeft;
                 let y = e.clientY - e.target.offsetTop;
@@ -41,8 +76,15 @@
                 ripples.style.left = x + "px";
                 ripples.style.top = y + "px";
                 btnMore.appendChild(ripples);
-
+                if (circleImage.src.endsWith("moon.png")) {
+                        ripples.style.backgroundColor="white";
+                }
+               cursor.style.display = 'none';
                 setTimeout(() => {
                     ripples.remove()
                 }, 1000);
             })
+
+             btnMore.addEventListener("mouseleave",()=>{
+                 cursor.style.display = 'block';
+             })
